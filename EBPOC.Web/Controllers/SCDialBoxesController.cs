4f8754d77903f8eb6f66b9836b33a0536b31ae53
Surajit Kumar Shah <surajitkumar.shah@cognizant.com>
2019-04-17 22:37:12 -0700

    Dynamic DialBox and Search folder SCSearchController updated

---
+++ EBPOC.Web/Controllers/SCDialBoxesController.cs
@@ -0,0 +1,30 @@
+﻿using EBPOC.Web.Models;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBPOC.Web.Controllers
+{
+    public class SCDialBoxesController : Controller
+    {
+
+        public ActionResult SCDialBox()
+        {
+            List<SCDialBoxVM> dialBoxs = new List<SCDialBoxVM>();
+            MultilistField multilistField = Sitecore.Context.Database.GetItem("{AE039C2F-0C6D-4C2B-A388-F61CCF6417A9}").Fields["DialBoxGroupItems"];
+            if (multilistField != null)
+            {
+                Item[] carouselItems = multilistField.GetItems();
+                foreach (Item item in carouselItems)
+                {
+                    dialBoxs.Add(new SCDialBoxVM(item));
+                }
+            }
+            return PartialView(dialBoxs);
+        }
+    }
+}
