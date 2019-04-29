5f4dfddb7281ed8a46b3d14fe2fc45de91349f00
Surajit Kumat Shah <surajitkumar.shah@cognizant.com>
2019-04-28 23:32:13 -0700

    Search change

--- EBPOC.Web/Controllers/SCSearchController.cs
+++ EBPOC.Web/Controllers/SCSearchController.cs
@@ -6,6 +6,7 @@ using Sitecore.ContentSearch.Linq;
 using Sitecore.ContentSearch.SearchTypes;
 using Sitecore.Data.Items;
 using Sitecore.Links;
+using Sitecore.Mvc.Common;
 using Sitecore.Mvc.Extensions;
 using Sitecore.Search;
 using System;
@@ -69,10 +70,10 @@ namespace EBPOC.Web.Controllers
         [HttpPost]
         public ActionResult Search(string searchStr, string[] facets)
         {
-            return View(new SearchResults(searchStr, facets));
-
-            //var url = LinkManager.GetItemUrl(Sitecore.Context.Database.GetItem("{07F9D462-768E-4126-ADB0-AFB0C2567B98}"));
-            //return Redirect(url);
+            //return View(new SearchResults(searchStr, facets));
+            ContextService.Get().GetCurrent<ViewContext>().ViewData.Add("_SharedModel", new SearchResults(searchStr, facets));
+            var url = LinkManager.GetItemUrl(Sitecore.Context.Database.GetItem("{07F9D462-768E-4126-ADB0-AFB0C2567B98}"));
+            return Redirect(url);
         }
 
         public ActionResult Results()
